fc94566f0db87709ab94aa6ea5fa8606f8c024f2
max <max.r@hdcorp.co.uk>
2024-06-12 17:00:59 +0100

    first commit

---
+++ status_checker/go.mod
@@ -0,0 +1,3 @@
+module github.com/maxrye1996/http_status_checker
+
+go 1.18
---
+++ status_checker/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	linksStr := os.Getenv("LINKS")
+	linksStr = strings.TrimSpace(linksStr)
+	links := strings.Split(linksStr, ",")
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go getWebsite(link, c)
+	}
+	for l := range c {
+		go func(l string) {
+			time.Sleep(5 * time.Second)
+			getWebsite(l, c)
+		}(l)
+	}
+}
+
+func getWebsite(l string, c chan string) {
+	url := formatURL(l)
+	_, err := http.Get(url)
+	if err != nil {
+		fmt.Println(l + " is down!")
+		c <- l
+		return
+	} else {
+		fmt.Println(l + " is up!")
+		c <- l
+	}
+}
+
+func formatURL(link string) string {
+	link = strings.TrimSpace(link)
+	if !strings.HasPrefix(link, "http://") && !strings.HasPrefix(link, "https://") {
+		link = "http://" + link
+	}
+	return link
+}
